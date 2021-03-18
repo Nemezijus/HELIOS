ed7998a82c507db86c4dd2ae652bbdf676887921
Nemezijus <aplauska@femtonics.eu>
2021-03-18 11:40:24 +0100

    Update moculus_createhdf5.m

--- h5/moculus_createhdf5.m
+++ h5/moculus_createhdf5.m
@@ -162,7 +162,7 @@ end
 if do_onacid
     data_locations = {MCpairs.motcorr};
     onacidloc = MCpairs(1).onacid;
-    onacidloc = strsplit(onacidloc,'\');
+    onacidloc = strsplit(onacidloc{1},'\');
     onacidloc = onacidloc(1:end-1);
     onacidloc = strjoin(onacidloc,'\');
     out = moculus_embedonacid(hdf5loc, data_locations, onacidloc, stageids); 
