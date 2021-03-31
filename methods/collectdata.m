5b5f23cc695c4d4b71864fc1a1622252bbfd07c8
Nemezijus <aplauska@femtonics.eu>
2021-03-31 12:09:24 +0200

    Update collectdata.m

--- methods/collectdata.m
+++ methods/collectdata.m
@@ -1,6 +1,6 @@
 function collectdata(setup, MC_ROI_PAIRS, stimlist)
 % collectdata(setup, MC_ROI_PAIRS, stimlist) - creates data.mat files from provided
-% pairs of motion corrected files and mescroifiles. If multiple mescroi 
+% pairs of motion corrected files and mescroifiles. If multiple mescroi
 % files are specified per one mot.corr. file then they are applied one by
 % one for each unit within the mot.corr. file.
 
@@ -23,33 +23,37 @@ for ip = 1:numel(mrp)
             else
                 Nmescroi = 1;
             end
-                mode='multi';
-                file_open([],mcfileloc);
-                r = mestaghandle('isf');
-                sz = size(r);
-                units = 1:sz(2);
-%                 units = 1:numel(behaviorloc);
+            mode='multi';
+            file_open([],mcfileloc);
+            r = mestaghandle('isf');
+            sz = size(r);
+            units = 1:sz(2);
+            %                 units = 1:numel(behaviorloc);
+            if isempty(stimlist)
+                for ic = 1:numel(units)
+                    if iscell(roifileloc)
+                        if Nmescroi < ic
+                            roiloc = roifileloc{end};
+                        else
+                            roiloc = roifileloc{ic};
+                        end
+                    else
+                        roiloc = roifileloc;
+                    end
+                    disp(['working on unit ',num2str(ic)]);
+                    out = AOExporterVR(mcfileloc,roiloc,[],saveloc,mode,units(ic));
+                    d(ic).data = out;
+                end
+                data=[];
+                for i=1:length(units)
+                    data=[data d(i).data];
+                end
+            else
                 data = AOExporter_HELIOS(mcfileloc,roifileloc,stimlist,saveloc,mode);
-%                 for ic = 1:numel(units)
-%                     if iscell(roifileloc)
-%                         if Nmescroi < ic
-%                             roiloc = roifileloc{end};
-%                         else
-%                             roiloc = roifileloc{ic};
-%                         end
-%                     else
-%                         roiloc = roifileloc;
-%                     end
-%                     disp(['working on unit ',num2str(ic)]);
-%                     out = AOExporterVR(mcfileloc,roiloc,[],saveloc,mode,units(ic));
-%                     d(ic).data = out;
-%                 end
-%                 data=[];
-%                 for i=1:length(units)
-%                     data=[data d(i).data];
-%                 end
-                disp('saving data.mat file');
-                save([saveloc '\data.mat'],'data','-v7.3')
+            end
+            
+            disp('saving data.mat file');
+            save([saveloc '\data.mat'],'data','-v7.3')
             
         case 'reso'
             if isonacid
