8a7bfd61143d5d3b07b59aa986c366363e41a94b
Nemezijus <aplauska@femtonics.eu>
2021-05-31 13:27:45 +0200

    Update multilayer_rois.m

--- utilities/multilayer_rois.m
+++ utilities/multilayer_rois.m
@@ -6,9 +6,9 @@ function multilayer_rois(PROJ, ROI, saveloc)
 
 % PROJ = multilayer_projections(mescpath, Nlayers, [1:5]);
 Nlayers = numel(PROJ);
-a = 90;
+a = 90;%rotation angle in degrees
 sz = size(PROJ(1).mean);
-r = [cosd(90) -sind(90); sind(90) cosd(90)]; %rotation matrix
+r = [cosd(a) -sind(a); sind(a) cosd(a)]; %rotation matrix
 for ilayer = 1:Nlayers
     figure;
     set(gcf,'units', 'normalized', 'position', [0.322 0.159 0.392 0.625]);
@@ -17,11 +17,22 @@ for ilayer = 1:Nlayers
     for iR = 1:numel(R)
         x = R(iR).pseudo(1,:) - sz(1)/2;
         y = R(iR).pseudo(2,:) - sz(2)/2;
+        x_c = R(iR).pseudo_centroid(2) - sz(1)/2;
+        y_c = R(iR).pseudo_centroid(1) - sz(2)/2;
+        
         interm = [x;y];
+        interm_c = [x_c;y_c];
+        
         c = interm'*r;
+        cc = interm_c'*r;
+        
         x = c(:,1) + sz(1)/2;
         y = c(:,2) + sz(2)/2;
+        
+        x_c = cc(1) + sz(1)/2;
+        y_c = cc(2) + sz(2)/2;
         plot(x, y,'w-'); hold on
+        plot(x_c, y_c,'ro','MarkerSize',3); hold on
     end
     saveas(gcf, [saveloc,'\contours_',num2str(ilayer)]);
     close(gcf);
